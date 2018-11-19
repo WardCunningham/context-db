b51bead29b91fe7c2c9ea9f9766756d9ef7c1b95
Ward Cunningham <ward@newrelic.com>
2018-11-19 09:48:53 -0800

    source details as params to cypher

--- load_json.sh
+++ load_json.sh
@@ -5,10 +5,15 @@ SITE=http://context.asia.wiki.org
 SLUG=$1
 echo $SLUG
 
-JSON=`curl -s $SITE/plugin/json/$SLUG`
+LINK="$SITE/$SLUG.html"
 CYPHER=`curl -s $SITE/$SLUG.json | jq '.story[]|select(.type=="code").text'`
-POST="{\"statements\":[{\"statement\":$CYPHER, \"parameters\": {\"json\":$JSON}}]}"
+DESC=`curl -s $SITE/$SLUG.json | jq '.story[]|select(.type=="json").text'`
+DATE=`curl -s $SITE/$SLUG.json | jq '.story[]|select(.type=="json").written/1000|strftime("%Y-%m-%d")'`
+SOURCE=`echo null | jq -c "{name:\"$SLUG\",run:$DATE,description:$DESC,links:\"$LINK\"}"`
+echo "$SOURCE"
 
+JSON=`curl -s $SITE/plugin/json/$SLUG`
+POST="{\"statements\":[{\"statement\":$CYPHER, \"parameters\": {\"json\":$JSON,\"props\":$SOURCE}}]}"
 DB=http://0.0.0.0:7474
 HEAD="-H accept:application/json -H content-type:application/json"
 time curl -s $HEAD -d "$POST" -X POST "$DB/db/data/transaction/commit" | \
